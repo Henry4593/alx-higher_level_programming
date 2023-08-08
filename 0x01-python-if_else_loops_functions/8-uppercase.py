0a1fbe8f0e1f41a102709766fe19156756a7b408
Henry4593 <jonyango4@gmail.com>
2023-08-08 19:39:46 +0300

    Added the following dependency file(s): and README.md

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     final_str = ""
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            char = chr(ord(char) - 32)
-            final_str += char
-    print("{}".format(final_str))
+    for idx_str in range(len(str)):
+        if (ord(str[idx_str]) >= 97 and ord(str[idx_str]) <= 122):
+            final_str += chr(ord(str[idx_str]) - 32)
+            continue
+        final_str += str[idx_str]
+
+    print('{0}'.format(final_str))
