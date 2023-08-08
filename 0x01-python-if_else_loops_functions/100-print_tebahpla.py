ead079ca5c4f995503a97611a0c60397d63f11a1
Henry4593 <jonyango4@gmail.com>
2023-08-09 01:42:26 +0300

    Added the following dependency file(s): lists.h and README.md

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 final_str = ""
-for idx_str in reversed(range(97, 123)):
-    if (idx_str % 2 == 0):
+for idx_str in range(122, 96, -1):
+    if idx_str % 2 == 0:
         final_str += chr(idx_str)
     else:
         final_str += chr(idx_str - 32)
