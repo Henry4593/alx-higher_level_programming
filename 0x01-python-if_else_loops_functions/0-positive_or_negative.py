9fabb3dd63c7e9cada7be3ec4d6667b8717e8c7b
Henry4593 <jonyango4@gmail.com>
2023-08-08 20:19:35 +0300

    Added the following dependency file(s): and README.md

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-gen_rdm_number = number
-if number == 0:
-    print(f"{0:d} is zero".format(gen_rdm_number))
-elif number < 0:
-    print(f"{0:d} is negative".format(gen_rdm_number))
+gen_num = number
+if gen_num == 0:
+    print("{0:d} is zero".format(gen_num))
+elif gen_num > 0:
+    print("{0:d} is positive".format(gen_num))
 else:
-    print(f"{0:d} is positive".format(gen_rdm_number))
+    print("{0:d} is negative".format(gen_num))
