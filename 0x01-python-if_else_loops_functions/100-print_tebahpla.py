9d2fbc92b42cd30116085a1c58dc739f066fd560
Henry4593 <jonyango4@gmail.com>
2023-08-09 01:07:58 +0300

    Added the following dependency file(s): lists.h and README.md

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-final_str = ""
-ascii_no = ord("z")
-upper_on = False
-while ascii_no >= ord("a") and ascii_no >= ord("A"):
-	if upper_on:
-		ascii_no -= 32
-		final_str += chr(ascii_no)
-		ascii_no += 31
-		upper_on = False
-	else:
-		final_str += chr(ascii_no)
-		ascii_no -= 1
-		upper_on = True
-print(final_str, end='')
+for idx_str in reversed(range(97, 123)):
+    if (idx_str % 2 == 0):
+        print(chr(idx_str), end='')
+    else:
+        print(chr(idx_str - 32), end='')
