6eada8d130774844907eadefe81d9f5e478486a5
Henry4593 <jonyango4@gmail.com>
2023-08-09 00:53:11 +0300

    Solved all the tasks on the project '0x01-python-if_else_loops_functions'

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
 
-fizzbuzz()
-print("")
+final_str = ""
+ascii_no = ord("z")
+upper_on = False
+while ascii_no >= ord("a") and ascii_no >= ord("A"):
+	if upper_on:
+		ascii_no -= 32
+		final_str += chr(ascii_no)
+		ascii_no += 31
+		upper_on = False
+	else:
+		final_str += chr(ascii_no)
+		ascii_no -= 1
+		upper_on = True
+print(final_str, end='')
