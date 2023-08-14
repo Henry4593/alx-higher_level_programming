b6ef10291497af6bc122c4d2027cf787bb285853
Henry4593 <jonyango4@gmail.com>
2023-08-15 02:10:57 +0300

    solved the problem on project 0x03-python-data_structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
     for idx_reverse in my_list[::-1]:
-        print("{}".format(idx_reverse))
+        print("{:d}".format(idx_reverse))
