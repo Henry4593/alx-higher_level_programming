04baa596ae9a346aed49bfb7677a7a74440e3f34
Henry4593 <jonyango4@gmail.com>
2023-08-15 02:17:54 +0300

    solved the problem on project 0x03-python-data_structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = 0
-    first_char = sentence[0]
     if sentence == "":
-        first_char = None
-        return (length, first_char)
-    else:
-        if first_char is None:
-            first_char = None
-        length = len(sentence)
-    return (length, first_char)
+        return (0, None)
+    return (len(sentence), sentence[0])
