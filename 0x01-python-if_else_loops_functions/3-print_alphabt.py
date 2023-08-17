f50a77d3d3f7c5be732e9284aa82748ec6e3fc7f
Henry4593 <112892733+Henry4593@users.noreply.github.com>
2023-08-17 16:13:07 +0300

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for alpha_lowercase in range(97, 123):
-    if (alpha_lowercase == 101) or (alpha_lowercase == 113):
-        continue
-    print("{:c}".format(alpha_lowercase), end='')
