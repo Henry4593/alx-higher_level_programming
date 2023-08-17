e015d3dc7a9aca733bc1e8ec5ed66707ed460b4e
Henry4593 <112892733+Henry4593@users.noreply.github.com>
2023-08-17 16:14:49 +0300

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/4-new_in_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def new_in_list(my_list, idx, element):
-    my_list_cpy = [i for i in my_list]
-    if idx < 0 or idx > len(my_list)-1:
-        return my_list_cpy
-    else:
-        my_list_cpy[idx] = element
-        return (my_list_cpy)
--- 0x03-python-data_structures/README.md
+++
@@ -1,1 +0,0 @@
-This is a readme file for projects on 0x03-python-data_structures
