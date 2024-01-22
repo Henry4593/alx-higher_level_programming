9ab1ecedfee49d6b10b092966d02052a2dad5b5c
Henry4593 <jonyango4@gmail.com>
2024-01-22 17:45:29 +0300

    Solved all the tasks on the project '0x05-python-exceptions'

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,8 @@ def safe_print_division(a, b):
         result_div = a / b
     except ZeroDivisionError:
         result_div = None
+    except TypeError:
+        result_div = None
     finally:
         print("Inside result_div: {0}".format(result_div))
         return result_div
