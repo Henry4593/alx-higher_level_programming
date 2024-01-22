78e5a9c5846f9801cfce8c210e14f578d8b0b40c
Henry4593 <jonyango4@gmail.com>
2024-01-22 23:27:14 +0300

    added code snippet

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
     try:
         result = float(a) / b
     except (ZeroDivisionError, TypeError, OverflowError, ValueError):
