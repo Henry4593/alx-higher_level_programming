bc9420b1a5a510a39415e6d578215fbc7ab9492f
Henry4593 <jonyango4@gmail.com>
2024-02-05 17:23:39 +0300

    Solved all the tasks on the project '0x0A-python-inheritance'

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,11 @@
 """Defines a function to check if an object is an exact instance of a class.
 
-This function takes two arguments:
-- `obj`: The object to check.
-- `a_class`: The class to compare the type of `obj` to.
+    This function takes two arguments:
+    - `obj`: The object to check.
+    - `a_class`: The class to compare the type of `obj` to.
 
-It returns `True` if `obj` is an exact instance of `a_class`, and `False`
-otherwise.
+    It returns `True` if `obj` is an exact instance of `a_class`, and `False`
+    otherwise.
 """
 
 
