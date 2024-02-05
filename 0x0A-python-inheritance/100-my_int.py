2eab47f952dc8aae0bc26e6e4ef1d89183a376c7
Henry4593 <jonyango4@gmail.com>
2024-02-05 16:48:00 +0300

    Solved all the tasks on the project '0x0A-python-inheritance'

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,10 +9,10 @@ class MyInt(int):
        equality and inequality operators.
     """
 
-    def __equal__(self, value):
+    def __eq__(self, value):
         """Adjusts the == operator to function like the != operator."""
         return self.real != value
 
-    def __notEqual__(self, value):
+    def __ne__(self, value):
         """Adjusts the != operator to function like the == operator."""
         return self.real == value
