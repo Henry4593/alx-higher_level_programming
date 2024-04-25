cf37b388c950fc02616e58e62197b62dee335006
Henry4593 <jonyango4@gmail.com>
2024-04-25 16:55:04 +0300

    fixed bugs

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,20 +17,9 @@ def find_peak(list_of_integers):
     Returns:
         The value of a peak element in the list, or None if no peak is found.
     """
-    if list_of_integers is None or list_of_integers == []:
+    lint = list_of_integers
+    if lint:
+        lint.sort()
+        return lint[-1]
+    else:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
